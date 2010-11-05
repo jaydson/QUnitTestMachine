00beb13e8a00243bb554cbf3d372f8e7c57d28ec
Jaydson Gomes <jayalemao@gmail.com>
2010-11-05 16:28:31 -0200

    Issue 1: Added possibility to set multiple paths to load scripts. Docs updated.

--- config.js
+++ config.js
@@ -4,26 +4,18 @@
 $(document).ready(function(){
 
     // Set App that QUnitTestMachine must to run
-    QUnitTestMachine.defaults.path = 'http://localhost/Javascript/appteste/';
+    QUnitTestMachine.defaults.path = 'http://localhost:81/';
 
     // Set a file test prefix
     QUnitTestMachine.defaults.prefix = 'test_';
     
     // Just add some JS files that we must to test
-    QUnitTestMachine.scripts.add('script1.js','script2.js');
-    /*QUnitTestMachine.scripts.add({
+    QUnitTestMachine.scripts.add({
         "scripts": [
-            {name: "script4.js"},
-            {name: "script5.js"},
-            {name: "script6.js"},
-            {name: "script7.js"},
-            {name: "script8.js"},
-            {name: "script9.js"},
-            {name: "script10.js"},
-            {name: "script11.js"},
-            {name: "script12.js"},
-            {name: "script13.js"}]
-    });*/
+            {name: "app/js/myscript.js"},
+            {name: "app2/myscript2.js"}
+		]
+    });
 
     // Init
     QUnitTestMachine.init();
